72e2d9befd1d68f339a3994ab6b8149eacbf7cfa
Tasnim <tasneem.almousa@live.com>
2025-04-01 06:35:16 +0300

    Initial Commit

---
+++ Quranian/QuranianApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuranianApp.swift
+//  Quranian
+//
+//  Created by Tasnim Almousa on 01/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct QuranianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuranianTests/QuranianTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuranianTests.swift
+//  QuranianTests
+//
+//  Created by Tasnim Almousa on 01/04/2025.
+//
+
+import Testing
+@testable import Quranian
+
+struct QuranianTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
